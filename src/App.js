2650849c6839066efce85414b16a5feb87838ea7
mikesnacki <mikesnacki@gmail.com>
2019-10-13 09:46:11 -0400

    Snacki rents on MERN stack

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "../src/style.scss";
+import Header from './components/Header'
+import Home from "./components/Home"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header/>
+        <Route path="/" exact component={Home}/>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,49 @@
+import React, { useState, useRef } from "react"
+import { useWindowDimensions } from "../utilhooks/useWindowDim"
+import useOnClickOutside from "../utilhooks/useOnClickOutside"
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+
+  const { width } = useWindowDimensions();
+  const collapseWidth = 1000;
+  const headerRef = useRef();
+
+  const [navDisplay, activateNavDisplay] = useState(false)
+
+  useOnClickOutside(headerRef, () => activateNavDisplay(false))
+
+  const links = [
+    <Link key={1} className="header-text nav-links" to="/">Home</Link>,
+    <Link key={2} className="header-text nav-links" to="/FetchProperty">Experience</Link>,
+    <Link key={3} className="header-text nav-links" to="/Admin">Admin</Link>,    
+  ]
+
+  return (
+
+    <div ref={headerRef}>
+      <header className="header space-between">
+        <h1 className="header-text">Snacki Rents</h1>
+        {width >= collapseWidth
+          ? <ul className="nav-links">{links}</ul >
+          :
+          <button
+            className={`menu-bar-${navDisplay}`}
+            onClick={() => activateNavDisplay(!navDisplay)}
+          >
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </button>
+        }
+      </header>
+      <ul
+        onMouseLeave={() => activateNavDisplay(!navDisplay)}
+        onClick={() => activateNavDisplay(!navDisplay)}
+        className={`menu-dropdown-${navDisplay}`}>
+        {navDisplay === true && width < collapseWidth && links}
+      </ul>
+    </div >
+
+  )
+}
