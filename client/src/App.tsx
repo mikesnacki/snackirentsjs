186edab2b3d0fad7fa3ffe9dd6d7660c3c0ad2b1
mikesnacki <mikesnacki@gmail.com>
2019-10-23 18:41:06 -0400

    Added code splitting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, 
         Route } from "react-router-dom";
 import "../src/style.scss";
 import Header from './components/Header'
 import Home from "./components/Home"
-import Properties from "./components/Properties"
-import Login from "./components/Login"
-import AdminForm from './components/Admin';
+import Loading from "./components/Loading"
+
+const Properties = React.lazy(()=> import("./components/Properties"))
+const Login = React.lazy(()=> import("./components/Login"))
+const AdminForm = React.lazy(()=> import('./components/Admin'))
 
 function App() {
 
   return (
     <div>
-      <Router>
-        <Header/>
-        <Route path="/" exact component={Home}/>
-        <Route path="/properties" exact component={Properties}/>
-        <Route path="/login" component={Login}/>
-        <Route path="/admin" component={AdminForm}/>
-      </Router>
+      <Suspense fallback={<Loading/>}>
+        <Router>
+          <Header/>
+          <Route path="/" exact component={Home}/>
+          <Route path="/properties" exact component={Properties}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/admin" component={AdminForm}/>
+        </Router>
+      </Suspense>
     </div>
   );
 }
