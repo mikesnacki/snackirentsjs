95e1d5a133b478326ff367702be197993ea65164
mikesnacki <mikesnacki@gmail.com>
2019-10-24 16:21:07 -0400

    ???

--- server/server.js
+++ server/server.js
@@ -23,9 +23,9 @@ mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
 const connection = mongoose.connection;
 
 if (process.env.NODE_ENV==="production"){
-    app.use(express.static('client/build'));
+    app.use(express.static('../client/build'));
     app.get("*", (req, res)=>{
-        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+        res.sendFile(path.join(__dirname, "../client/build/index.html"));
     })
 }
 
