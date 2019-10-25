c4a53f1ba410266311a68ba3f2990809244efbba
mikesnacki <mikesnacki@gmail.com>
2019-10-25 15:03:59 -0400

    added cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-// const cors = require("cors");
+const cors = require("cors");
 const path=require("path")
 const routes = require("./routes/routes.js")
 const config = require("config");
@@ -15,7 +15,7 @@ mongoose
     .then(()=> console.log("MongoDB Connected"))
     .catch(err=> console.log(err))
 
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes)
